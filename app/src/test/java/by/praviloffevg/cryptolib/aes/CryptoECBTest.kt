0c8838e6d1091a71fd36953b2f88f2bf5dde4194
Evgenij Pravilov <praviloffevg@gmail.com>
2019-02-12 22:35:47 +0300

    Added tests for ECB encryption mode

---
+++ app/src/test/java/by/praviloffevg/cryptolib/aes/CryptoECBTest.kt
@@ -0,0 +1,60 @@
+package by.praviloffevg.cryptolib.aes
+
+import assertk.assertions.isEqualTo
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class CryptoECBTest {
+    private val initialString = "initialString"
+    private val key = "key"
+
+    private val byteKeyGenerator = ByteKeyGenerator(AesKeySpecification.AES256)
+    private val cryptoEcb= CryptoECB(byteKeyGenerator)
+
+    @Test
+    fun shouldEncryptAndDecryptIntoStringGivenString() {
+        val encryptedString = cryptoEcb.encrypt(initialString, key)
+
+        val decryptedString = cryptoEcb.decryptIntoString(encryptedString, key)
+
+        assertk.assert(decryptedString).isEqualTo(initialString)
+    }
+
+    @Test
+    fun shouldEncryptAndDecryptIntoStringGivenByteArray() {
+        val encryptedString = cryptoEcb.encrypt(initialString.toByteArray(), key)
+
+        val decryptedString = cryptoEcb.decryptIntoString(encryptedString, key)
+
+        assertk.assert(decryptedString).isEqualTo(initialString)
+    }
+
+    @Test
+    fun shouldEncryptAndDecryptIntoByteArrayGivenString() {
+        val encryptedString = cryptoEcb.encrypt(initialString, key)
+
+        val decrypted = cryptoEcb.decryptIntoByteArray(encryptedString, key)
+
+        assertk.assert(String(decrypted)).isEqualTo(initialString)
+    }
+
+    @Test
+    fun shouldEncryptAndDecryptIntoByteArrayGivenByteArray() {
+        val encryptedString = cryptoEcb.encrypt(initialString.toByteArray(), key)
+
+        val decrypted = cryptoEcb.decryptIntoByteArray(encryptedString, key)
+
+        assertk.assert(String(decrypted)).isEqualTo(initialString)
+    }
+
+    @Test
+    fun shouldEncrypt() {
+        val expectedString = "R7TY6Qn2+bjknjuBWBuUJw==\n"
+
+        val encryptedString = cryptoEcb.encrypt(initialString, key)
+
+        assertk.assert(encryptedString).isEqualTo(expectedString)
+    }
+}
