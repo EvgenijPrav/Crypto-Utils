717c3bb1f1e6b8a27044388cdd5f89442aa5eec7
Yauheni Pravilau <praviloffevg@gmail.com>
2019-06-21 23:47:19 +0300

    Removed empty lines

--- app/src/main/java/by/praviloffevg/cryptolib/rsa/KeyValidationException.kt
+++ app/src/main/java/by/praviloffevg/cryptolib/rsa/KeyValidationException.kt
@@ -1,12 +1,8 @@
 package by.praviloffevg.cryptolib.rsa
 
-
-
-
-class KeyValidationException (code: ExceptionCode ,message: String) : Exception(message) {
-        enum class ExceptionCode {
+class KeyValidationException(val code: ExceptionCode, override val message: String) : Exception(message) {
+    enum class ExceptionCode {
         KEY_NOT_FOUND,
         KEY_EXPIRED
     }
-
 }
