a2fe99d40cf7a4efdabf6e5a4af224801f36f7ea
Yauheni Pravilau <praviloffevg@gmail.com>
2019-06-21 15:53:11 +0300

    Fixed some magic numbers reported by Detekt

--- app/src/main/java/by/praviloffevg/cryptolib/HashUtil.kt
+++ app/src/main/java/by/praviloffevg/cryptolib/HashUtil.kt
@@ -7,15 +7,19 @@ import java.security.MessageDigest
 
 object HashUtil {
 
+    private const val RADIX = 16
+    private const val RIGHT_SHIFT = 4
+    private const val LEFT_SHIFT = 1
+
     fun getHash(input: String, algorithm: Algorithms): String {
         val messageDigest = MessageDigest.getInstance(algorithm.type)
         messageDigest.reset()
         messageDigest.update(input.toByteArray())
         val bytes = messageDigest.digest()
-        val sb = StringBuilder(bytes.size shl 1)
+        val sb = StringBuilder(bytes.size shl LEFT_SHIFT)
         for (aByte in bytes) {
-            sb.append(Character.forDigit(aByte.toInt() and 0xf0 shr 4, 16))
-            sb.append(Character.forDigit((aByte.toInt() and 0x0f), 16))
+            sb.append(Character.forDigit(aByte.toInt() and 0xf0 shr RIGHT_SHIFT, RADIX))
+            sb.append(Character.forDigit((aByte.toInt() and 0x0f), RADIX))
         }
         return sb.toString()
     }
