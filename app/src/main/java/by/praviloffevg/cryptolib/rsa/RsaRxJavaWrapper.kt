3436cb27f1b8a2497b70f13d8ff01d297c908847
Yauheni Pravilau <praviloffevg@gmail.com>
2019-07-19 15:41:22 +0300

    Added annotations for RsaRx wrapper, fixed scheduling

--- app/src/main/java/by/praviloffevg/cryptolib/rsa/RsaRxJavaWrapper.kt
+++ app/src/main/java/by/praviloffevg/cryptolib/rsa/RsaRxJavaWrapper.kt
@@ -1,6 +1,7 @@
 package by.praviloffevg.cryptolib.rsa
 
 import android.content.Context
+import androidx.annotation.WorkerThread
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -12,40 +13,46 @@ import java.security.PublicKey
 
 object RsaRxJavaWrapper {
 
-    private val computationScheduler = Schedulers.computation()
-
+    @WorkerThread
     fun getRsaInstance(context: Context, keyProperties: KeyProperties): Observable<RsaProvider> {
-        return Observable.just<RsaProvider>(RsaProvider(context, keyProperties))
-            .subscribeOn(computationScheduler)
+        return Observable.just(RsaProvider(context, keyProperties))
+            .subscribeOn(Schedulers.computation())
     }
 
+    @WorkerThread
     fun generateNewKey(rsaProvider: RsaProvider): Completable =
         Completable.fromAction { Single.just(rsaProvider.createNewKeys()) }
-            .subscribeOn(computationScheduler)
+            .subscribeOn(Schedulers.computation())
 
+    @WorkerThread
     fun deleteKeys(rsaProvider: RsaProvider): Completable =
         Completable.fromAction { Single.just(rsaProvider.deleteKey()) }
-            .subscribeOn(computationScheduler)
+            .subscribeOn(Schedulers.computation())
 
+    @WorkerThread
     fun getPublicKey(rsaProvider: RsaProvider): Single<PublicKey> =
         Single.just(rsaProvider.getPublicKey())
-            .subscribeOn(computationScheduler)
+            .subscribeOn(Schedulers.computation())
 
+    @WorkerThread
     fun isKeyExpired(rsaProvider: RsaProvider): Single<Boolean> =
         Single.just(rsaProvider.isKeyExpired())
-            .subscribeOn(computationScheduler)
+            .subscribeOn(Schedulers.computation())
 
+    @WorkerThread
     fun encrypt(rsaProvider: RsaProvider, messageToEncrypt: String): Single<String> =
             Single.just(rsaProvider.encrypt(messageToEncrypt))
-                .subscribeOn(computationScheduler)
+                .subscribeOn(Schedulers.computation())
 
+    @WorkerThread
     fun encryptWithProvidedPublicKey(rsaProvider: RsaProvider,
                                      messageToEncrypt: String,
                                      publicKey: PublicKey): Single<String> =
         Single.just(rsaProvider.encryptWithProvidedPublicKey(messageToEncrypt, publicKey))
-            .subscribeOn(computationScheduler)
+            .subscribeOn(Schedulers.computation())
 
+    @WorkerThread
     fun decrypt(rsaProvider: RsaProvider,decryptedMessage: String): Single<String> =
             Single.just(rsaProvider.decrypt(decryptedMessage))
-                .subscribeOn(computationScheduler)
+                .subscribeOn(Schedulers.computation())
 }
